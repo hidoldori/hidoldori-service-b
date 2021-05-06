e55105cf45174f0c9c6c82f564c4e18934f646a0
hidoldori <hidoldori@gmail.com>
2021-05-06 17:05:30 +0900

    first commit

---
+++ src/main/java/com/example/hidoldoriserviceb/HidoldoriServiceBApplication.java
@@ -0,0 +1,13 @@
+package com.example.hidoldoriserviceb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HidoldoriServiceBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HidoldoriServiceBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hidoldoriserviceb/HidoldoriServiceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hidoldoriserviceb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HidoldoriServiceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
